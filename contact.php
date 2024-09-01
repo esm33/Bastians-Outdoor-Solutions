bb121b15f55b4426d27beb254254229e3865d3ba
esm33 <esm33@njit.edu>
2024-09-01 19:42:36 -0400

    form

--- contact.php
+++ contact.php
@@ -1,20 +1,25 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
-    
-    $to = "bastiansoutdoorsolutions@gmail.com"; // Replace with your email address
-    $subject = "New Message from $name";
-    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-    $headers = "From: $email";
+// Get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Message sent! Check your email.";
-    } else {
-        echo "Message sending failed.";
-    }
-} else {
-    echo "Invalid request.";
+$to = "bastiansoutdoorsolutions@gmail.com";
+$subject = "This is the subject line";
+
+// The following text will be sent
+// Name = user entered name
+// Email = user entered email
+// Message = user entered message 
+$txt ="Name = ". $name . "\r\n  Email = " 
+    . $email . "\r\n Message =" . $message;
+
+$headers = "From: noreply@demosite.com" . "\r\n" .
+            "CC: somebodyelse@example.com";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
 }
+
+// Redirect to
+header("Location:last.html");
 ?>
